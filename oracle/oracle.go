3ac7cc9b8ba5f8927c800988695d55239209f4c8
koromo <koromowonder@gmail.com>
2021-11-03 00:06:36 +0700

    Add coingecko oracle

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -3,10 +3,22 @@ package oracle
 import (
 	"context"
 	"net/url"
+	"sort"
+	"time"
 )
 
+// CryptoOracle represents price oracle for cryptocurrency
 type CryptoOracle interface {
-	GetQuoteItems(ctx context.Context, targetCryptoSymbols []string) []QuoteItem
+	GetQuoteItems(ctx context.Context, targetCryptoSymbols []string) ([]QuoteItem, error)
+}
+
+// QuoteItem represents currency/token quote information
+type QuoteItem struct {
+	Symbol      string
+	Name        string
+	Slug        string
+	LastUpdated time.Time
+	USDPrice    float32
 }
 
 type query struct {
@@ -27,4 +39,11 @@ func buildURLWithQueryParams(baseURL string, queries []query) (string, error) {
 	url.RawQuery = params.Encode()
 
 	return url.String(), nil
+}
+
+// sortQuoteItems (alphabetically asc)
+func sortQuoteItems(quoteItems []QuoteItem) {
+	sort.Slice(quoteItems, func(i, j int) bool {
+		return quoteItems[i].Symbol < quoteItems[j].Symbol
+	})
 }
